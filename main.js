7127680977d06fa891dbeb6ff4edc231c5e26ebe
BFGDoesGithub <86089724+BFGDoesGithub@users.noreply.github.com>
2022-01-27 18:41:58 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var song1 = "";
 var song2 = "";
 
+
 scoreRightWrist = 0;
 scoreLeftWrist = 0;
 
@@ -30,4 +31,25 @@ function setup() {
 
 function draw() {
 	image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+	console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results)
+		scoreRightWrist =  results[0].pose.keypoints[10].score;
+		scoreLeftWrist = results[0].pose.keypoints[9].score;
+		console.log("SRW = " + scoreRightWrist + "SLW = " + scoreLeftWrist);
+
+		rightWristX = results[0].pose.rightWrist.x;
+		rightWristY = results[0].pose.rightWrist.y;
+		console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+		leftWristX = results[0].pose.leftWrist.x;
+		leftWristY = results[0].pose.leftWrist.y;
+		console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+	}
 }
